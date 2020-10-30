f33761648e40a9e1fc64dc6de92dc2f83fd2d9e4
Chris Adams <cpadams@gmail.com>
2020-10-30 00:34:58 -0700

    adding 'energy to battery today' metric

--- modbus-schneider-mppt80-600.py
+++ modbus-schneider-mppt80-600.py
@@ -6,7 +6,8 @@ schneider_registers = [
   ['4E - Input DC Current',0x004E, 'U32', 'FIX3'],
   ['50 - Input DC Power',0x0050, 'U32', 'RAW'],
   ['70 - Energy From PV This Hour (kWh)', 0x0070, 'U32', 'FIX3'],
-  ['74 - Energy From PV Today (kWh)', 0x0074, 'U32', 'FIX3']
+  ['74 - Energy From PV Today (kWh)', 0x0074, 'U32', 'FIX3'],
+  ['8C - Energy to Battery Today (kWh)', 0x008C, 'U32', 'FIX3']
 ]
 
 # scan is not used for Schneider inverters but solariot.py expects it to exist
